951f922be81ac9a2ced25a55f737e349e0f77781
Robert Snyder <r.snyder49@gmail.com>
2018-09-04 19:43:36 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,6 @@
 class User 
-  attr_reader :last_name, :first_name 
-  attr_writer :last_name, :first_name
+  attr_accessor :last_name, :first_name 
+  
   
   def initialize(first_name, last_name) 
     @first_name = first_name 
